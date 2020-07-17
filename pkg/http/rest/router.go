ec6a60c98484f60e3b10b160d46a984d22ded889
Yofrank <yofr4nk@gmail.com>
2020-07-17 16:21:16 -0400

    Add get-multipart-upload endpoint

--- pkg/http/rest/router.go
+++ pkg/http/rest/router.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"github.com/yofr4nk/s3-chunked-upload-builder/pkg/http/handlers"
 	"net/http"
 )
 
@@ -15,6 +16,8 @@ func RouterHandler() http.Handler {
 		w.WriteHeader(http.StatusOK)
 		_, _ = fmt.Fprintf(w, "pong")
 	}).Methods("GET")
+
+	router.HandleFunc("/get-multipart-upload", handlers.CreateMultipartUpload).Methods("POST")
 
 	handler := cors.AllowAll().Handler(router)
 
